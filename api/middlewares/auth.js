028ddce9b88775f6ee87b74117f1ca4f1ab20d77
Ahmed Samir Morsy <ahmedmorsy97@users.noreply.github.com>
2020-11-19 19:53:40 +0400

    remove x-auth

--- api/middlewares/auth.js
+++ api/middlewares/auth.js
@@ -28,7 +28,7 @@ export const authorize = (userTypes) => (req, res, next) => {
 };
 
 export const authenticate = (req, res, next) => {
-  const token = req.cookies.userLogin || req.header("x-auth");
+  const token = req.cookies.userLogin;
 
   User.findByToken(token)
     .then((user) => {
